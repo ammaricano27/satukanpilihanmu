2127ef55c44880427c9870cf13b6491515f5322c
Marr <ammarabdul27@gmail.com>
2024-11-22 22:01:36 +0700

    fixed ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./public/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            primary: "#242F9B",
+            gradient: "#646FD4",
+            dark: "#111827",
+        }
+    },
   },
   plugins: [],
 }
